7af97d2dfdb0efb6da48e9c19ce05d09e0d7d191
Cameron Kaiser <classilla@floodgap.com>
2018-07-06 17:49:00 -0700

    closes #502: CVE-2016-6293/7415 CVE-2017-7867/7868/14952/15422

--- intl/icu/source/i18n/zonemeta.cpp
+++ intl/icu/source/i18n/zonemeta.cpp
@@ -681,7 +681,6 @@ ZoneMeta::createMetazoneMappings(const UnicodeString &tzid) {
                     mzMappings = new UVector(deleteOlsonToMetaMappingEntry, NULL, status);
                     if (U_FAILURE(status)) {
                         delete mzMappings;
-                        deleteOlsonToMetaMappingEntry(entry);
                         uprv_free(entry);
                         break;
                     }
