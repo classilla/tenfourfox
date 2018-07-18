30dbc914e8567c27a72064236df1e0fbee9e336d
Cameron Kaiser <classilla@floodgap.com>
2018-07-17 21:08:19 -0700

    #514: some additional checks

--- 104fx_copy.sh
+++ 104fx_copy.sh
@@ -2,15 +2,17 @@
 
 # from AuroraFox, modified for TenFourFox
 
-set ppath=$1
+set ppath="$1"
 if ("$ppath" == "/" || "$ppath" == "") then
 	echo 'invalid path'
 	exit
 endif
+if (-e "$ppath/Contents/MacOS/firefox") then
+	rm -rf "$ppath" || exit
+endif
 
 set verbose
-rm -rf $ppath || echo 'Never mind.'
-cp -RL obj-ff-dbg/dist/TenFourFox.app $ppath || cp -RL obj-ff-dbg/dist/TenFourFoxDebug.app $ppath || exit
+cp -RL obj-ff-dbg/dist/TenFourFox.app "$ppath" || cp -RL obj-ff-dbg/dist/TenFourFoxDebug.app "$ppath" || exit
 cd $ppath/Contents/MacOS || exit
 
 # determine which libgcc got linked (default to /opt/local/lib/gcc48)
