f059d57c0700213874da111f27243f18ce8718f5
Cameron Kaiser <classilla@floodgap.com>
2018-10-29 20:39:22 -0700

    #531: pref CSS error reporting to false except for DEBUG builds

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2236,7 +2236,11 @@ pref("layout.word_select.stop_at_punctuation", true);
 pref("layout.selection.caret_style", 0);
 
 // pref to report CSS errors to the error console
+#if DEBUG
 pref("layout.css.report_errors", true);
+#else
+pref("layout.css.report_errors", false);
+#endif
 
 // Should the :visited selector ever match (otherwise :link matches instead)?
 pref("layout.css.visited_links_enabled", true);
