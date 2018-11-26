de66c97dac991bd6de0f02dd616052c9994c1b2c
Cameron Kaiser <classilla@floodgap.com>
2018-11-25 21:35:58 -0800

    closes #535: add TLD update to upcert script and pull up for testing

--- 104fx_upcerts.sh
+++ 104fx_upcerts.sh
@@ -12,5 +12,6 @@ endif
 set verbose
 cp ../esr60/security/nss/lib/ckfw/builtins/certdata.txt security/nss/lib/ckfw/builtins/certdata.txt
 cp ../esr60/security/manager/ssl/StaticHPKPins.h security/manager/ssl/StaticHPKPins.h
+cp ../esr60/netwerk/dns/effective_tld_names.dat netwerk/dns/effective_tld_names.dat
 perl ./104fx_import_esr60_stspreload.pl > security/manager/ssl/nsSTSPreloadList.inc
 
