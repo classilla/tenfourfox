2d99b19d79808035f4a8c842c02be17d9c2a06ed
Cameron Kaiser <classilla@floodgap.com>
2020-04-29 23:14:18 -0700

    #600: new tzdata, tzdata updater

---
+++ 104fx_uptzdata.sh
@@ -0,0 +1,12 @@
+#!/bin/csh -f
+
+set root=https://raw.githubusercontent.com/unicode-org/icu/master/icu4c/source/data/misc
+set verbose
+cd intl/icu/source/data/misc || exit
+
+curl -L ${root}/metaZones.txt > metaZones.txt
+curl -L ${root}/timezoneTypes.txt > timezoneTypes.txt
+curl -L ${root}/windowsZones.txt > windowsZones.txt
+curl -L ${root}/zoneinfo64.txt > zoneinfo64.txt
+
+
