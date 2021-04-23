d354aa4a7d6fcb8494e5f7949a3d9d2748eeed44
wicknix <39230578+wicknix@users.noreply.github.com>
2021-04-22 23:51:07 -0500

    Branding Changes

--- browser/branding/nightly/pref/firefox-branding.js
+++ browser/branding/nightly/pref/firefox-branding.js
@@ -2,23 +2,22 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-pref("startup.homepage_override_url","http://www.tenfourfox.com/start/%LOCALE%/%APP%/%VERSION%/whatsnew/from/%OLD_VERSION%");
-pref("startup.homepage_welcome_url","http://www.tenfourfox.com/start/%LOCALE%/%APP%/%VERSION%/firstrun/");
+pref("startup.homepage_override_url","about:home");
+pref("startup.homepage_welcome_url","about:");
 pref("startup.homepage_welcome_url.additional", "");
 // The time interval between checks for a new version (in seconds)
 pref("app.update.interval", 86400); // 24 hours
 // The time interval between the downloading of mar file chunks in the
 // background (in seconds)
-// 0 means "download everything at once"
-pref("app.update.download.backgroundInterval", 0);
-// Give the user x seconds to react before showing the big UI. default=12 hours
-pref("app.update.promptWaitTime", 43200);
+pref("app.update.download.backgroundInterval", 60);
+// Give the user x seconds to react before showing the big UI. default=24 hours
+pref("app.update.promptWaitTime", 86400);
 // URL user can browse to manually if for some reason all update installation
 // attempts fail.
-pref("app.update.url.manual", "http://www.tenfourfox.com/");
+pref("app.update.url.manual", "");
 // A default value for the "More information about this update" link
 // supplied in the "An update is available" page of the update wizard. 
-pref("app.update.url.details", "http://www.tenfourfox.com/");
+pref("app.update.url.details", "");
 
 // The number of days a binary is permitted to be old
 // without checking for an update.  This assumes that
@@ -35,8 +34,8 @@ pref("browser.search.param.yahoo-fr-ja", "mozff");
 
 // Number of usages of the web console or scratchpad.
 // If this is less than 5, then pasting code into the web console or scratchpad is disabled
-pref("devtools.selfxss.count", 5);
+pref("devtools.selfxss.count", 0);
 
 // After bug 876037
-pref("app.releaseNotesURL", "http://www.tenfourfox.com/relnotes/%VERSION%/%LOCALE%/");
-pref("app.vendorURL", "http://www.tenfourfox.com/");
+pref("app.releaseNotesURL", "about:support");
+pref("app.vendorURL", "https:/github.com/wicknix/");
